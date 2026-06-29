2ed82f6587336cb3bbb5f4b66820c371530bad97
ROA-NOA <su2anozi@uwaterloo.ca>
2026-06-28 21:19:31 -0400

    Updated navlink buttons to have a subtle glow effect Created seperate class for the blue highlights on section cards Added function to delay page transitions for a smooth page closing effect when navigating.

--- style/s_master.js
+++ style/s_master.js
@@ -1,14 +1,35 @@
-const projectPanel = document.querySelectorAll(".projectPanel");
+//Click detection for homepage project panels
+const projectPanels = document.querySelectorAll(".projectPanel");
 
-projectPanel.forEach((projectPanel) => {
-    projectPanel.addEventListener("click", () => {
+projectPanels.forEach((panel) => {
+    panel.addEventListener("click", () => {
         removeActiveClasses();
-        projectPanel.classList.add("active");
+        panel.classList.add("active");
     });
 });
 
 const removeActiveClasses = () => {
-    projectPanel.forEach((projectPanel) => {
-        projectPanel.classList.remove("active");
+    projectPanels.forEach((panel) => {
+        panel.classList.remove("active");
     });
+};
+
+
+/* BUGGED: script not getting called for some reason? Will return when more awake.*/
+const pageTransition = document.querySelectorAll("a");
+
+pageTransition.forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        removeTransitionClass();
+        document.body.classList.add("page_close");
+        setTimeout(() => {
+                window.location.href = link.href;
+        }, 1000); // Match CSS transition duration
+
+    });
+});
+
+const removeTransitionClass = () => {
+        document.body.classList.remove("page_mask_horiz");
 };
